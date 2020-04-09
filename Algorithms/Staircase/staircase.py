77c5735e91f24c0ffd363320c839a3aeb9dc629e
Blake Goms <blakegoms@gmail.com>
2020-04-08 20:08:48 -0700

    added new problem

---
+++ Algorithms/Staircase/staircase.py
@@ -0,0 +1,7 @@
+# https://www.hackerrank.com/challenges/staircase/problem
+def staircase(n):
+    for i in range(1, n + 1):
+        # its saying give me n- 1 spaces, then add # i many times
+        print(' ' * (n - i) + '#' * i)
+        
+staircase(5)
