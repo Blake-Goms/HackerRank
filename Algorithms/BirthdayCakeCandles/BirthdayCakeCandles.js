088c7850e8cf27142f4b56bcb6741bf7c13ba53c
Blake Goms <blakegoms@gmail.com>
2020-04-13 12:52:03 -0700

    added new problem and js solution

---
+++ Algorithms/BirthdayCakeCandles/BirthdayCakeCandles.js
@@ -0,0 +1,14 @@
+function birthdayCakeCandles(ar) {
+    // use built in method to find max value, spread in array to avoid errors
+    const height = Math.max(...ar)
+    let count = 0
+    for(let i = 0; i < ar.length; i++){
+        if(ar[i] === height){
+            count += 1
+        }
+    }
+    console.log(count)
+    return count
+}
+const array = [4,3,2,1,4]
+birthdayCakeCandles(array)
